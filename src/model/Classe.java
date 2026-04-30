2d39a74d1efb5cf2f2fe751eea522772be70b3e0
lissouck <leprohorus@gmail.com>
2026-04-30 12:38:31 +0100

    modification

--- src/model/Classe.java
+++ src/model/Classe.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Classe {
-    
+    private int id;
+    private String nom;
+    private List<Eleve> eleves;
+    private Professeur professeur;
+
+    public Classe(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+        this.eleves = new ArrayList<>();
+    }
+
+    public void ajouterEleve(Eleve e) {
+        eleves.add(e);
+    }
+
+    public void setProfesseur(Professeur p) {
+        this.professeur = p;
+    }
+
+    public List<Eleve> getEleves() {
+        return eleves;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
--- src/model/Eleve.java
+++ src/model/Eleve.java
@@ -1,3 +1,22 @@
 public class Eleve {
-    
+    private int id;
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Eleve(int id, String nom, String prenom, int age) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
+    public int getAge() { return age; }
+
+    public void setNom(String nom) { this.nom = nom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+    public void setAge(int age) { this.age = age; }
 }
