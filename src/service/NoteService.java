7107b960c353c1f7a1ce6938082458a66c6e6476
Helderfox49 <mbottoguyzo2012@gmail.com>
2026-05-08 15:09:30 +0100

    Ajout de la classe NoteService

---
+++ src/service/NoteService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Eleve;
+import model.Matiere;
+import model.Note;
+import repository.NoteRepository;
+import util.Validateur;
+
+import java.util.List;
+
+/**
+ * Logique métier pour la gestion des notes.
+ * Contient aussi les calculs de moyenne et la génération de bulletins.
+ *
+ * @author Module Notes
+ * @version 1.0
+ */
+public class NoteService {
+
+}
